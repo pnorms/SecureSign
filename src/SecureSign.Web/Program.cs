37ba05c90c9b66636f76aee2697c79d64dc3b354
Patrick Norman <pNorman@elliottmgmt.com>
2019-05-06 15:59:40 -0400

    Listen on all

--- src/SecureSign.Web/Program.cs
+++ src/SecureSign.Web/Program.cs
@@ -15,7 +15,7 @@ namespace SecureSign.Web
 	{
 		public static void Main(string[] args)
 		{
-			CreateWebHostBuilder(args).Build().Run();
+			CreateWebHostBuilder(args).UseUrls("http://0.0.0.0:5000/").Build().Run();
 		}
 
 		public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
